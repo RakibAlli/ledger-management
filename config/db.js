7c256727fe122d0353bc426464bab5da1b6dbe52
rakib <rakiballis@webmob.com>
2024-06-13 08:50:35 +0530

    change db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
-const MongoDB = mongoose.connect('mongodb+srv://rakib:Rakib%401999@cluster0.bwxs7wf.mongodb.net/ledger-management', { useUnifiedTopology: true });
-mongoose.connection.once('open', () => {
+const dbUri = 'mongodb+srv://rakib:Rakib%401999@cluster0.bwxs7wf.mongodb.net/ledger-management';
+
+mongoose.connect(dbUri, {
+    useUnifiedTopology: true
+}).then(() => {
     console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Exit the process with failure
 });
 
-module.exports = MongoDB;
+module.exports = mongoose;
